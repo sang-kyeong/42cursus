25e7a6aea491164dda81973c5e9587c97ea1732e
Sangkyeong Kim <sangkkim@c4r4s8.42seoul.kr>
2023-01-04 19:19:34 +0900

    change Libft to submodule

--- Circle4/CPP_Module_04/ex02/Color.hpp
+++ Circle4/CPP_Module_04/ex02/Color.hpp
@@ -1,6 +1,6 @@
 # ifndef __COLOR_H__
 #  define __COLOR_H__
-#  define NORMAL	"\033[0;30m"
+#  define NORMAL	"\033[0;0m"
 #  define RED		"\033[0;31m"
 #  define GREEN		"\033[0;32m"
 #  define YELLOW	"\033[0;33m"
